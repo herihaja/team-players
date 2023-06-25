5fbb654af603523f71a594954101f7582452e352
herihaja <hery.imiary@gmail.com>
2023-06-25 23:37:07 +0300

    Show navbar on mobile

--- assets/app.js
+++ assets/app.js
@@ -6,19 +6,14 @@
  */
 
 import { createApp } from "vue";
-import App from "./App.vue";
-import store from "./store";
 import router from "./router";
 import "./assets/css/nucleo-icons.css";
 import "./assets/css/nucleo-svg.css";
-import SoftUIDashboard from "./soft-ui-dashboard";
+import "./assets/scss/soft-ui-dashboard.scss";
 import PlayerIndex from "./components/PlayerIndex.vue";
-
 
 const appInstance = createApp(
     PlayerIndex
 );
-//appInstance.use(store);
 appInstance.use(router);
-//appInstance.use(SoftUIDashboard);
 appInstance.mount("#players");
