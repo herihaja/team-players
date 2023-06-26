16b378bbdfa3d2fa2c69da4fed035d861baf80bd
herihaja <hery.imiary@gmail.com>
2023-06-26 07:23:37 +0300

    Fixing navigation link from pagination

--- assets/components/Pagination.vue
+++ assets/components/Pagination.vue
@@ -4,30 +4,23 @@ export default {
     props: ['currentPage', 'lastPage', 'total'],
     methods: {
         getPaginationLink: (page) => {
-            //console.log(lastPage);
             return `/player/json/?page=${page}`;
         },
         range: (start, end) => {
             return Array.from({ length: end - start + 1 }, (_, index) => index + start);
         },
         onChildClick (event) {
-            //$emit.onClick();
             const link = event.target.getAttribute('data-link');
-            // console.log('Clicked item ID:', link);
             this.$emit('clickedPageLink', {'link': link, 'event': event});
         }
     },
-    mounted: () => {
-       // console.log(this.lastPage);
-       // console.error(this.currentPage);
-    }
 }
 </script>
 <template>
     <nav v-if="lastPage >= 1">
         <ul class="pagination justify-content-center">
             <li :class="['page-item', currentPage <= 1 ? 'disabled' : '']">
-                <a class="page-link" :href="getPaginationLink(currentPage - 1)" aria-label="Previous" :onClick="onChildClick" >
+                <a class="page-link" href="javascript:void(0);" :data-link="getPaginationLink(currentPage - 1)" aria-label="Previous" :onClick="onChildClick" >
                     &laquo; 
                 </a>
             </li>
@@ -35,7 +28,7 @@ export default {
                 <a class="page-link" href="javascript:void(0);" :data-link="getPaginationLink(page)" :onClick="onChildClick">{{ page }}</a>
             </li>
             <li :class="['page-item', currentPage >= lastPage ? 'disabled' : '']">
-                <a class="page-link" :href="getPaginationLink(currentPage + 1)" aria-label="Next" :onClick="onChildClick">
+                <a class="page-link" href="javascript:void(0);" :data-link="getPaginationLink(currentPage + 1)" aria-label="Next" :onClick="onChildClick">
                     &raquo;
                 </a>
             </li>
